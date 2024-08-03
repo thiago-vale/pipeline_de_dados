87cf48c989763a1ddec62a802927014582a075d5
thiago-vale <consultoria.thiagovale@gmail.com>
2024-08-03 11:06:01 -0300

    mod Dag

--- dags/spark_dag.py
+++ dags/spark_dag.py
@@ -4,7 +4,7 @@ from airflow.operators.bash import BashOperator
 
 default_args = {
     'owner': 'Thiago',
-    'depends_on_past': False,
+    'depends_on_past': True,
     'start_date': datetime(2023, 1, 1),
     'email_on_failure': False,
     'email_on_retry': False,
@@ -17,36 +17,32 @@ dag = DAG(
     default_args=default_args,
     description='A simple DAG to run Spark ETL script',
     schedule_interval='@daily',
+    catchup=False,
+    max_active_runs=1,
 )
 
 source_to_landing = BashOperator(
     task_id='source_to_landing',
-    bash_command='python /home/thiago/Documentos/GitHub/pipeline_de_dados/src/00_source_to_landing.py',
+    bash_command='python3 /home/thiago/Documentos/GitHub/pipeline_de_dados/src/00_source_to_landing.py',
     dag=dag,
 )
 
 landing_to_bronze = BashOperator(
     task_id='landing_to_bronze',
-    bash_command='python /home/thiago/Documentos/GitHub/pipeline_de_dados/src/01_landing_to_bronze.py',
+    bash_command='python3 /home/thiago/Documentos/GitHub/pipeline_de_dados/src/01_landing_to_bronze.py',
     dag=dag,
 )
 
 bronze_to_silver = BashOperator(
     task_id='bronze_to_silver',
-    bash_command='python /home/thiago/Documentos/GitHub/pipeline_de_dados/src/02_bronze_to_silver.py',
+    bash_command='python3 /home/thiago/Documentos/GitHub/pipeline_de_dados/src/02_bronze_to_silver.py',
     dag=dag,
 )
 
 silver_to_gold = BashOperator(
     task_id='silver_to_gold',
-    bash_command='python /home/thiago/Documentos/GitHub/pipeline_de_dados/src/03_silver_to_gold.py',
+    bash_command='python3 /home/thiago/Documentos/GitHub/pipeline_de_dados/src/03_silver_to_gold.py',
     dag=dag,
 )
 
-gold_to_modeled = BashOperator(
-    task_id='gold_to_modeled',
-    bash_command='python /home/thiago/Documentos/GitHub/pipeline_de_dados/src/04_gold_to_modeled.py',
-    dag=dag,
-)
-
-source_to_landing >> landing_to_bronze >> bronze_to_silver >> silver_to_gold >> gold_to_modeled
+source_to_landing >> landing_to_bronze >> bronze_to_silver >> silver_to_gold
